ebc5488d6ecdab1d487c8a0a07b69ee9221aad36
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2025-09-30 14:50:28 +0100

    XG: separate out journal api to a separate file.

--- src/main.py
+++ src/main.py
@@ -1,32 +1,17 @@
-from datetime import datetime, timezone
-import uuid
-from typing import Any
-
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Any
 
+from journal import router as journal_router
+
+
+app = FastAPI()
+app.include_router(journal_router)
 
 class Item(BaseModel):
     name: str
     price: float
     is_offer: bool | None = None
-
-class Weahter(BaseModel):
-    description: str
-    temperature: float
-
-class JournalEntryInput(BaseModel):
-    id: uuid.UUID = uuid.uuid4()
-    location: str
-    note: str
-
-class JournalEntry(JournalEntryInput):
-    weather: Weahter | None = None
-    created_at: datetime = datetime.now(timezone.utc)
-    updated_at: datetime | None = None
-
-app = FastAPI()
-
 
 @app.get("/")
 async def home() -> dict[str, str]:
@@ -42,6 +27,4 @@ async def read_item(item_id: int, q: str | None = None) -> dict:
 async def update_item(item_id: int, item: Item) -> dict[str, Any]:
     return {"item_id": item_id, "item": item}
 
-@app.post("/journal/entries", status_code=201)
-async def create_travel_journal_entry(entry: JournalEntryInput) -> JournalEntry:
-    return JournalEntry(**entry.model_dump(), weather=None)
+
