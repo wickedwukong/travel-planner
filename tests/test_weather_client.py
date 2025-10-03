5f8cdf84235bab7e902d1202cd83e5a6723ff99e
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2025-10-03 19:20:29 +0100

    XG: dependency inject httpx.AsyncClient so we can manage it life cycle together with the app life cycle and also take an advantage of its connection pooling

--- tests/test_weather_client.py
+++ tests/test_weather_client.py
@@ -4,16 +4,19 @@ import httpx
 from journal.models import Weather
 import pytest
 
+
 class WeatherAPIClient:
-    def __init__(self, api_key: str, base_url: str) -> None:
+    def __init__(
+        self, api_key: str, base_url: str, http_client: httpx.AsyncClient
+    ) -> None:
         self.api_key = api_key
         self.base_url = base_url
+        self.http_client = http_client
 
     async def weather(self, city: str) -> Weather:
         url = f"{self.base_url.rstrip('/')}/v1/current.json?key={self.api_key}&q={city}"
 
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url)
+        response = await self.http_client.get(url)
 
         weather_json = response.json()
 
@@ -27,9 +30,13 @@ class WeatherAPIClient:
 @pytest.mark.asyncio
 async def test_weather_client() -> None:
     weather_api_key: str = os.environ["WEATHER_API_KEY"]
-    weather_api_client: WeatherAPIClient = WeatherAPIClient(api_key=weather_api_key, base_url="https://api.weatherapi.com")
-
-    weather = await weather_api_client.weather("London")
+    async with httpx.AsyncClient() as client:
+        weather_api_client: WeatherAPIClient = WeatherAPIClient(
+            api_key=weather_api_key,
+            base_url="https://api.weatherapi.com",
+            http_client=client,
+        )
+        weather = await weather_api_client.weather("London")
 
     assert weather.city == "London"
     assert weather.temperature is not None
