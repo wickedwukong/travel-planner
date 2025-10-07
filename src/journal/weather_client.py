411ab041d818979599327e67cb2e36d24d6f7f0a
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2025-10-07 18:01:39 +0100

    XG: move weather client to prod code.

---
+++ src/journal/weather_client.py
@@ -0,0 +1,23 @@
+from journal.models import Weather
+import httpx
+
+class WeatherAPIClient:
+    def __init__(
+        self, api_key: str, base_url: str, http_client: httpx.AsyncClient
+    ) -> None:
+        self.api_key = api_key
+        self.base_url = base_url
+        self.http_client = http_client
+
+    async def weather(self, city: str) -> Weather:
+        url = f"{self.base_url.rstrip('/')}/v1/current.json?key={self.api_key}&q={city}"
+
+        response = await self.http_client.get(url)
+
+        weather_json = response.json()
+
+        return Weather(
+            city=weather_json["location"]["name"],
+            description=weather_json["current"]["condition"]["text"],
+            temperature=weather_json["current"]["temp_c"],
+        )
--- tests/test_weather_client.py
+++ tests/test_weather_client.py
@@ -1,30 +1,9 @@
 import os
 
 import httpx
-from journal.models import Weather
 import pytest
 
-
-class WeatherAPIClient:
-    def __init__(
-        self, api_key: str, base_url: str, http_client: httpx.AsyncClient
-    ) -> None:
-        self.api_key = api_key
-        self.base_url = base_url
-        self.http_client = http_client
-
-    async def weather(self, city: str) -> Weather:
-        url = f"{self.base_url.rstrip('/')}/v1/current.json?key={self.api_key}&q={city}"
-
-        response = await self.http_client.get(url)
-
-        weather_json = response.json()
-
-        return Weather(
-            city=weather_json["location"]["name"],
-            description=weather_json["current"]["condition"]["text"],
-            temperature=weather_json["current"]["temp_c"],
-        )
+from journal.weather_client import WeatherAPIClient
 
 
 @pytest.mark.asyncio
@@ -37,6 +16,7 @@ async def test_weather_client() -> None:
             http_client=client,
         )
         weather = await weather_api_client.weather("London")
+    print(weather)
 
     assert weather.city == "London"
     assert weather.temperature is not None
